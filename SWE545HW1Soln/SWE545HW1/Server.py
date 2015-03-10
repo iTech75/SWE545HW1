a8ee33f180fe69bae510b9193945a97b5ffc90d8
iTech75 <tekinaytekin@hotmail.com>
2015-03-11 00:48:35 +0200

    start

--- SWE545HW1Soln/SWE545HW1/Server.py
+++ SWE545HW1Soln/SWE545HW1/Server.py
@@ -1,6 +1,15 @@
 import threading
 import socket
 
+class ServerThread(threading.Thread):
+    def __init__(self, clientSocket, clientAddress):
+        self.clientSocket = clientSocket
+        self.clientAddress = clientAddress
+
+    def run(self):
+        while True:
+            self.clientSocket.re
+
 class Server(threading.Thread):
     """description of class"""
     def __init__(self):
@@ -8,8 +17,14 @@ class Server(threading.Thread):
         self.socket = socket.socket()
 
     def run(self):
-        self.socket.bind(self.socket.
+        self.socket.bind((socket.gethostname(), 12345))
+        self.socket.listen(5)
         print("Server started")
+        while True:
+            clientSocket, clientAddress = self.socket.accept()
+
+            print "Clint accepted"%clientAddress
+
         print("Server ended")
 
     def parser(self, request):
